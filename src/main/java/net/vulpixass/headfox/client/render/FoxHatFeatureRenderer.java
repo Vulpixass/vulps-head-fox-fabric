3be8cb069dcd3414e765c4e4a1796196db953dc0
Vulpixass <maxderfuchs165@gmail.de>
2026-02-17 20:45:08 +0100

    Initial Commit

--- src/main/java/net/vulpixass/headfox/client/render/FoxHatFeatureRenderer.java
+++ src/main/java/net/vulpixass/headfox/client/render/FoxHatFeatureRenderer.java
@@ -43,42 +43,40 @@ public class FoxHatFeatureRenderer extends FeatureRenderer<PlayerEntityRenderSta
         MinecraftClient client = MinecraftClient.getInstance();
         PlayerEntity me = client.player;
         fox_baby fox = this.foxModel;
+        PlayerEntity target = (PlayerEntity) client.world.getEntityById(state.id);
 
         if (me == null) return;
+        if (target == null || !target.getUuid().equals(me.getUuid())) {return;}
         ItemStack helmet = me.getEquippedStack(EquipmentSlot.HEAD);
         boolean hasHelmet = !helmet.isEmpty();
-        for (PlayerEntity p : client.world.getPlayers()) {
-            if (p.getUuid().equals(me.getUuid())) {
-                matrices.push();
-                this.getContextModel().head.applyTransform(matrices);
-                matrices.translate(0.0F, -1.1F, 0.0F);
-                matrices.scale(0.5F, 0.5F, 0.5F);
-                long time = client.world.getTime();
-                if (time >= nextTwitchTime) {
-                    twitchAmount = 0.3F;
-                    nextTwitchTime = (time + 20 + client.world.random.nextInt(10))*20;
-                }
-                twitchAmount *= 0.8F;
-                float breathe = 1.0F + (float)(Math.sin(time * 0.1F) * 0.025F);
-                foxModel.bb_main.xScale = breathe;
-                foxModel.bb_main.yScale = breathe;
-                foxModel.bb_main.zScale = breathe;
-                float growth = (breathe - 1.0F);
-                if (hasHelmet) {matrices.translate(0.0, -0.098, 0.0);}
-                matrices.scale(breathe, breathe, breathe);
-                matrices.translate(0.0F, -growth * 1.2F, 0.2F);
-
-                foxModel.head.yaw = twitchAmount * (client.world.random.nextBoolean() ? 1 : -1);
-                foxModel.head.pitch = twitchAmount * (client.world.random.nextBoolean() ? 1 : -1);
-                float sway = (float)Math.sin(time * (2 * Math.PI / 200.0)) * 0.10F;
-                sway = Math.max(-0.15F, Math.min(0.15F, sway));
-                foxModel.head.roll = sway;
-
-                //Render Fox
-                fox.head.resetTransform();
-                FeatureRenderer.render(foxModel, FOX_TEXTURE, matrices, queue, light, state, 0xFFFFFF, 0);
-                matrices.pop();
-            }
+        matrices.push();
+        this.getContextModel().head.applyTransform(matrices);
+        matrices.translate(0.0F, -1.1F, 0.0F);
+        matrices.scale(0.5F, 0.5F, 0.5F);
+        long time = client.world.getTime();
+        if (time >= nextTwitchTime) {
+            twitchAmount = 0.3F;
+            nextTwitchTime = (time + 20 + client.world.random.nextInt(10))*20;
         }
+        twitchAmount *= 0.8F;
+        float breathe = 1.0F + (float)(Math.sin(time * 0.1F) * 0.025F);
+        foxModel.bb_main.xScale = breathe;
+        foxModel.bb_main.yScale = breathe;
+        foxModel.bb_main.zScale = breathe;
+        float growth = (breathe - 1.0F);
+        if (hasHelmet) {matrices.translate(0.0, -0.098, 0.0);}
+        matrices.scale(breathe, breathe, breathe);
+        matrices.translate(0.0F, -growth * 1.2F, 0.2F);
+
+        foxModel.head.yaw = twitchAmount * (client.world.random.nextBoolean() ? 1 : -1);
+        foxModel.head.pitch = twitchAmount * (client.world.random.nextBoolean() ? 1 : -1);
+        float sway = (float)Math.sin(time * (2 * Math.PI / 200.0)) * 0.10F;
+        sway = Math.max(-0.15F, Math.min(0.15F, sway));
+        foxModel.head.roll = sway;
+
+        //Render Fox
+        fox.head.resetTransform();
+        FeatureRenderer.render(foxModel, FOX_TEXTURE, matrices, queue, light, state, 0xFFFFFF, 0);
+        matrices.pop();
     }
 }
